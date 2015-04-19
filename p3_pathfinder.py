da2801110243f8bc945ba461622cd2b1bdc3f58f
xxxPeace <rtan9@ucsc.edu>
2015-04-19 15:26:27 -0700

    finish  Dijkstra's
    finish  Dijkstra's

--- p3_pathfinder.py
+++ p3_pathfinder.py
@@ -1,5 +1,4 @@
-
-import math
+from math import sqrt
 from heapq import heappush, heappop
 
 
@@ -9,19 +8,20 @@ def find_path(source, destination, mesh):
 	path = []
 	detail_points = {}
 	startBox = None
+
 	print(source)
 	print(destination)
+
 	for box in mesh['boxes']:
 		if source[0] >= box[0] and source[0] <= box[1] and source[1] >= box[2] and source[1] <= box[3]:
-	#		path.append(box)
 			startBox = box
-			#detail_points[startBox] = source
 	queue = [startBox]
 	prev[startBox] = None
 
 	while queue:
 		discBox = heappop(queue)
 		if destination[0] >= discBox[0] and destination[0] <= discBox[1] and destination[1] >= discBox [2] and destination[1] <= discBox[3]: 
+			destination_box = discBox
 			break
 		neighbors = mesh['adj'][discBox]
 		for next_box in neighbors:
@@ -30,22 +30,27 @@ def find_path(source, destination, mesh):
 				heappush(queue,(next_box))
 
 	if destination[0] >= discBox[0] and destination[0] <= discBox[1] and destination[1] >= discBox [2] and destination[1] <= discBox[3]: 
-
 		node = discBox
 		while node != startBox:
 			path.append(node)
 			node = prev[node]
-		#path.append(startBox)
 		path.reverse()
 		prevPoint = source
-		for box in path:			
-			detail_points[box]= (prevPoint,(box[0],box[2]))
-			prevPoint = ((box[0],box[2]))
-		detail_points[box]= (prevPoint,destination)
-		#print(detail_points.values())
+		for box in path:
+			smallDist = {}
+			smallDist[(box[0],box[2])] = sqrt(prevPoint[0]*box[0] + prevPoint[1]*box[2])
+			smallDist[(box[1],box[2])] = sqrt(prevPoint[0]*box[1] + prevPoint[1]*box[2])
+			smallDist[(box[0],box[3])] = sqrt(prevPoint[0]*box[3] + prevPoint[1]*box[3])
+			smallDist[(box[1],box[3])] = sqrt(prevPoint[0]*box[1] + prevPoint[1]*box[3])
+			detail_points[box]= (prevPoint,(smallDist.keys()[0][0],smallDist.keys()[0][1]))
+			if box != destination_box:
+				prevPoint = ((smallDist.keys()[0][0],smallDist.keys()[0][1]))
+		detail_points[destination_box]= (prevPoint,destination)
 		return detail_points.values(), prev.keys()
 	else:
+		print ('no path')
 		return detail_points.values(), prev.keys()
+		
 	#		print (startBox)
 	#		print (mesh['adj'][box])
 	#		visited_nodes.append(box)
